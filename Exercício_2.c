b45d7aaf15d2d84cf3079d6e284e95b292e15184
Thalesrcs <thalesrebecchi@usp.br>
2026-05-18 17:11:35 -0300

    Exercício 2 Python 1

---
+++ Exercício_2.c
@@ -0,0 +1,4 @@
+altura = float(input('Qual inputa altura da parede? '))
+largura = float(input('Qual a largura da parede? '))
+print('A área da parede é de: {}', format(altura * largura))
+print('A quantidade de tinta necessária é: ', altura * largura / 2)
