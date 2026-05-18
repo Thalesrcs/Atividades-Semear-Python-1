c11e34cc18476ec31361ab45fae9e00d97e2585b
Thalesrcs <thalesrebecchi@usp.br>
2026-05-18 17:13:05 -0300

    Exercício 4  Python 1

---
+++ Exercício_4.c
@@ -0,0 +1,13 @@
+peso = float(input('Qual o seu peso? '))
+altura = float(input('Qual sua altura? '))
+imc = peso / (altura * altura)
+if imc < 18.5:
+    print('Abaixo do peso')
+elif 18.5 <= imc <= 25:
+    print('Peso ideal')
+elif 25 < imc <= 30:
+    print('Sobrepeso')
+elif 30 < imc <= 40:
+    print('Obesidade')
+elif imc > 40:
+    print('Obesidade mórbida')
